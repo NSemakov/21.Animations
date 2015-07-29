75432270ff8c951debf6ff2b578adffb9a4b60b4
Nikita Semakov <n.semakov@yahoo.com>
2015-07-29 17:23:49 +0300

    Initial Commit

---
+++ 21.UIViewAnimations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  21.UIViewAnimations
+//
+//  Created by Admin on 29.07.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
